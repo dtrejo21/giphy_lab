f43cd51ebb5ef5a09b1f107821926577481c5bb7
Daisy Trejo-Hernandez <dtrejohernandez@meta.com>
2023-06-14 11:37:01 -0700

    Fixed null error with eventlistener

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log("Hello")
+
+var submitBtn = document.getElementById("submitBtn");
+
+//This will be used if you're only calling a function once
+submitBtn.addEventListener("click", (event) => 
+{
+    event.preventDefault(); //Prevent form submission (optional)
+
+    //Perform your desired action here
+    console.log("Button clicked!");
+});
+
+
+/*
+submitBtn.addeventListener("click", fetchGiphyImages);
+function fetchGiphyImages(event)
+{
+    event.preventDeafult(); //prevent form submission(optional)
+    
+    //Perform your desired action here
+    console.log("Button clicked!");
+}*/
